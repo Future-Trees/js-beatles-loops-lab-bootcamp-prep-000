449f77788cf23b20560749d9671ba5f5d8e6cdb3
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-25 03:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ function theBeatlesPlay(musicians, instruments) {
   return arr
 }
 
-function johnLennonFacts(array){
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
 
-let i=0
-while (i < facts.length)
-    facts.push(`${facts[i]}!!!`)
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
     i++
   }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n){
+  const num = []
+  do 
+}
